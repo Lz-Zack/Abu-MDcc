80d7a02f3d417b5054ee263e53894a30aae2c9b9
Zack ! <126048807+Lz-Zack@users.noreply.github.com>
2023-12-12 18:50:45 +0530

    Create ping.js

---
+++ plugins/ping.js
@@ -0,0 +1,17 @@
+const { Module} = require("../lib/");
+Module(
+  {
+    pattern: "ping",
+    fromMe: false,
+    desc: "To check ping",
+    type: "user",
+  },
+  async (message, match) => {
+    const start = new Date().getTime();
+    await message.sendMessage("```Ping!```");
+    const end = new Date().getTime();
+    return await message.sendMessage(
+      "*Pong!*\n ```" + (end - start) + "``` *ms*"
+    );
+  }
+);
